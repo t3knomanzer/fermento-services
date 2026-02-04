0efc2a8dbca19056889d2ff070058d4d9e0efd3b
t3knomanzer <rhenares0@gmail.com>
2026-02-04 09:34:05 +0100

    Organizing shared packages and updating ingestor with new paths

--- ingestor/src/config.py
+++ ingestor/src/config.py
@@ -1,19 +1,11 @@
 import os
 
-
 API_ADDRESS = os.getenv("API_ADDRESS", "http://192.168.8.5:8091")
 
 BROKER_ADDRESS = os.getenv("BROKER_ADDRESS", "192.168.8.5")
 BROKER_PORT = os.getenv("BROKER_PORT", 1883)
 
-TOPIC_MQTT_FEEDING_SAMPLES_CREATE = "fermento/+/feeding_samples/create"
-TOPIC_MQTT_JARS_CREATE = "fermento/+/jars/create"
-TOPIC_MQTT_FEEDING_EVENTS_REQUEST = "fermento/+/feeding_events/request"
-TOPIC_MQTT_FEEDING_EVENTS_RECEIVE = "fermento/feeding_events/receive"
-
-SUBSCRIBE_TOPICS = [
-    TOPIC_MQTT_FEEDING_SAMPLES_CREATE,
-    TOPIC_MQTT_JARS_CREATE,
-    TOPIC_MQTT_FEEDING_EVENTS_REQUEST,
-    TOPIC_MQTT_FEEDING_EVENTS_RECEIVE,
-]
+TOPIC_FEEDING_SAMPLES_CREATE = "fermento/+/feeding_samples/create"
+TOPIC_JARS_CREATE = "fermento/+/jars/create"
+TOPIC_FEEDING_EVENTS_REQUEST = "fermento/+/feeding_events/request"
+TOPIC_FEEDING_EVENTS_RECEIVE = "fermento/feeding_events/receive"
--- ingestor/src/main.py
+++ ingestor/src/main.py
@@ -5,13 +5,10 @@ import config
 from fastapi import FastAPI
 
 from lib.api.client import APIClient
-from lib.mqtt.subscriber import MqttSubscriber
+from lib.mqtt.client import MqttSubscriber
 from lib.mqtt.utils import topic_matches_sub
-from fermento_schemas.ingestor.feeding_sample import FeedingSampleSchema
-from fermento_schemas.ingestor.jar import JarSchema
-from fermento_schemas.api.feeding_sample import FeedingSampleCreateSchema
-from fermento_schemas.api.jar import JarCreateSchema
-
+import fermento_embedded_schemas as mcu
+import fermento_service_schemas.api as api
 
 # ------------------------------------------------------
 # API Client Setup
@@ -24,12 +21,14 @@ api_client = APIClient(base_url=config.API_ADDRESS)
 # ------------------------------------------------------
 def on_message(topic, payload):
     print(f"Received {topic}: {payload}")
-    if topic_matches_sub(config.TOPIC_MQTT_FEEDING_SAMPLES_CREATE, topic):
+
+    if topic_matches_sub(config.TOPIC_FEEDING_SAMPLES_CREATE, topic):
         print("Topic found, processing data...")
         # Validate pydantic model
-        feeding_sample = FeedingSampleSchema.model_validate_json(payload)
+        feeding_sample = api.FeedingSampleCreateSchema.model_validate_json(payload)
+
         # Convert to api schema
-        feeding_sample_create = FeedingSampleCreateSchema(
+        feeding_sample_create = api.FeedingSampleCreateSchema(
             feeding_event_id=feeding_sample.feeding_event_id,
             temperature=feeding_sample.temperature,
             humidity=feeding_sample.humidity,
@@ -46,36 +45,41 @@ def on_message(topic, payload):
         except HTTPError as e:
             print(f"Error creating feeding sample: {e}")
 
-    elif topic_matches_sub(config.TOPIC_MQTT_JARS_CREATE, topic):
+    elif topic_matches_sub(config.TOPIC_JARS_CREATE, topic):
         print("Topic found, processing data...")
         # Process jar creation data
-        jar = JarSchema.model_validate_json(payload)
-        jar_create = JarCreateSchema(
+        jar = api.JarCreateSchema.model_validate_json(payload)
+
+        jar_create = api.JarCreateSchema(
             name=jar.name,
-            height=int(jar.distance),
+            height=int(jar.height),
         )
+
         # Send to api
         try:
             response = api_client.create_resource("jar", jar_create.model_dump())
             print(f"Jar created: {response}")
         except HTTPError as e:
             print(f"Error creating jar: {e}")
-    elif topic_matches_sub(config.TOPIC_MQTT_FEEDING_EVENTS_REQUEST, topic):
+
+    elif topic_matches_sub(config.TOPIC_FEEDING_EVENTS_REQUEST, topic):
         print("Topic found, processing data...")
-        response = api_client.list_resources("feeding-events")
-        if len(response) > 0:
-            subscriber.publish(
-                config.TOPIC_MQTT_FEEDING_EVENTS_RECEIVE, json.dumps(response)
-            )
+        try:
+            response = api_client.list_resources("feeding-events")
+        except HTTPError as e:
+            print(f"Error listing feeding events: {e}")
+            return
+
+        subscriber.publish(config.TOPIC_FEEDING_EVENTS_RECEIVE, json.dumps(response))
         print(f"Feeding events: {response}")
     else:
         print("Topic not implemented: " + topic)
 
 
 subscriber = MqttSubscriber(config.BROKER_ADDRESS, int(config.BROKER_PORT))
-subscriber.add_subscribe_topic(config.TOPIC_MQTT_FEEDING_SAMPLES_CREATE)
-subscriber.add_subscribe_topic(config.TOPIC_MQTT_JARS_CREATE)
-subscriber.add_subscribe_topic(config.TOPIC_MQTT_FEEDING_EVENTS_REQUEST)
+subscriber.add_subscribe_topic(config.TOPIC_FEEDING_SAMPLES_CREATE)
+subscriber.add_subscribe_topic(config.TOPIC_FEEDING_EVENTS_REQUEST)
+subscriber.add_subscribe_topic(config.TOPIC_JARS_CREATE)
 subscriber.add_on_message_callback(on_message)
 
 
@@ -97,11 +101,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+
 # ------------------------------------------------------
 # Sample Endpoint
 # ------------------------------------------------------
-
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
