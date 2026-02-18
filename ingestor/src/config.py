1bebed55b13f55db7961422e038f001902a8e394
t3knomanzer <rhenares0@gmail.com>
2026-02-18 21:01:39 +0100

    Restoring changes to ingestor config

--- ingestor/src/config.py
+++ ingestor/src/config.py
@@ -6,6 +6,7 @@ BROKER_ADDRESS = os.getenv("BROKER_ADDRESS", "192.168.8.5")
 BROKER_PORT = os.getenv("BROKER_PORT", 1883)
 
 TOPIC_FEEDING_SAMPLES_CREATE = "fermento/+/feeding_samples/create"
+TOPIC_FEEDING_SAMPLES_IMAGE = "fermento/+/feeding_samples/image/+"  # + for bundle_id
 TOPIC_JARS_CREATE = "fermento/+/jars/create"
 TOPIC_FEEDING_EVENTS_REQUEST = "fermento/+/feeding_events/request"
 TOPIC_FEEDING_EVENTS_RECEIVE = "fermento/feeding_events/receive"
