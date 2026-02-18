ef919064f299ba500131e68dd8c98722f9c40d2e
t3knomanzer <rhenares0@gmail.com>
2026-02-18 22:11:10 +0100

    Updating default addresses

--- ingestor/src/config.py
+++ ingestor/src/config.py
@@ -1,8 +1,8 @@
 import os
 
-API_ADDRESS = os.getenv("API_ADDRESS", "http://192.168.8.5:8091")
+API_ADDRESS = os.getenv("API_ADDRESS", "http://localhost:8091")
 
-BROKER_ADDRESS = os.getenv("BROKER_ADDRESS", "192.168.8.5")
+BROKER_ADDRESS = os.getenv("BROKER_ADDRESS", "13.50.219.203")
 BROKER_PORT = os.getenv("BROKER_PORT", 1883)
 
 TOPIC_FEEDING_SAMPLES_CREATE = "fermento/+/feeding_samples/create"
